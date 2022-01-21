200920f152ddaf65912f8c1110c1747c25422740
Sanjeevs23 <69767470+Sanjeevs23@users.noreply.github.com>
2022-01-21 16:44:00 +0530

    log added

--- src/test/java/com/cg/flightmgmt/FlightManagementSystemApplicationTests.java
+++ src/test/java/com/cg/flightmgmt/FlightManagementSystemApplicationTests.java
@@ -1,13 +1,29 @@
 package com.cg.flightmgmt;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.cg.flightmgmt.dao.AirportDao;
+import com.cg.flightmgmt.dto.Airport;
 
 @SpringBootTest
 class FlightManagementSystemApplicationTests {
+	@Autowired
+	@Mock
+	private AirportDao dao;
 
 	@Test
-	void contextLoads() {
+	public void testAddAirportMethod() {
+		
+	        Airport a = new Airport(1010, "XYZ Airport", "ABC");
+		Airport check = dao.addAirport(a);
+	        assertEquals(a.getAirportCode(), check.getAirportCode());
+	        assertEquals(a.getAirportName(), check.getAirportName());
+	        assertEquals(a.getAirportLocation(), check.getAirportLocation());
 	}
 
 }
